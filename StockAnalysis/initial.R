08fb0a8dbb6d307a6a5de24281b99d599bd3bb84
Tim <tmmtsmith@gmail.com>
2013-08-31 06:29:21 -0500

    Plotting added

--- StockAnalysis/initial.R
+++ StockAnalysis/initial.R
@@ -2,3 +2,9 @@ library(RODBC)
 scon <- odbcDriverConnect('driver={SQL Server};server=SERVER;database=StockAnalysis;trusted_connection=true')
 thdsma <- sqlQuery(scon, 'SELECT Date, Price, TwoHundredDaySMA FROM HistoricalData')
 summary(thdsma)
+
+# Plot data
+sox <- sqlQuery(scon, 'SELECT Date FROM HistoricalData ORDER BY ID ASC')
+soy <- sqlQuery(scon, 'SELECT Price FROM HistoricalData ORDER BY ID ASC')
+plot(as.factor(sox$Date), (soy$Price))
+odbcClose(scon)
