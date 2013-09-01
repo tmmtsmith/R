ba7bfad86699b20c162d14925fc27c0f020d449e
Tim <tmmtsmith@gmail.com>
2013-09-01 07:12:00 -0500

    Update initial.R

--- StockAnalysis/initial.R
+++ StockAnalysis/initial.R
@@ -1,3 +1,4 @@
+#initial
 library(RODBC)
 scon <- odbcDriverConnect('driver={SQL Server};server=SERVER;database=StockAnalysis;trusted_connection=true')
 thdsma <- sqlQuery(scon, 'SELECT Date, Price, TwoHundredDaySMA FROM HistoricalData')
@@ -6,5 +7,12 @@ summary(thdsma)
 # Plot data
 sox <- sqlQuery(scon, 'SELECT Date FROM HistoricalData ORDER BY ID ASC')
 soy <- sqlQuery(scon, 'SELECT Price FROM HistoricalData ORDER BY ID ASC')
-plot(as.factor(sox$Date), (soy$Price))
+plot(as.factor(sox$Date), (soy$Price), , main="Stock Price By Year", ylab="Price", xlab="Date", cex.lab=1, cex.main=1.5)
+# cex.main = font size of main title, cex.lab = font size of labels
+# compare:
+plot(as.factor(sox$Date), (soy$Price), ylab="Price", xlab="Date")
+title(main="Stock Price", col.main="Blue", font.main=4)
 odbcCloseAll()
+
+#installation - update to latest version
+install.packages("ggplot2")
